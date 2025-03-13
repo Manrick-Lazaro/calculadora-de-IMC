706dd55d76a75a2938fa07200f400cb244eb2e47
Manrick-Lazaro <manricksantos@gmail.com>
2025-03-13 14:57:22 -0300

    feat: Implementa a parte visual do form.

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,61 @@ class _HomeState extends State<Home> {
         ),
         backgroundColor: Colors.green,
         centerTitle: true,
-        actions: [IconButton(onPressed: () {}, icon: Icon(Icons.refresh))],
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.refresh, color: Colors.white),
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Icon(Icons.person_outlined, size: 120, color: Colors.green),
+            TextField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                label: Text('Peso (kg)'),
+                labelStyle: TextStyle(color: Colors.green),
+              ),
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.green, fontSize: 20),
+            ),
+            TextField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                label: Text('Altura (cm)'),
+                labelStyle: TextStyle(color: Colors.green),
+              ),
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.green, fontSize: 20),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10),
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.green,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(2),
+                  ),
+                  padding: EdgeInsets.symmetric(vertical: 10),
+                ),
+                child: Text(
+                  'Calcular',
+                  style: TextStyle(color: Colors.white, fontSize: 25),
+                ),
+              ),
+            ),
+            Text(
+              'data',
+              style: TextStyle(fontSize: 20, color: Colors.green),
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
       ),
     );
   }
